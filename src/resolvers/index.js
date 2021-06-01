9a6f1931fdf0e7e7c2201566eb4ad2ed8965d554
AmoDinho <u14284783@tuks.co.za>
2021-06-01 21:54:31 +0200

    commting everything

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,7 +1,11 @@
-import {hello} from "./query";
+import { hello } from "./query";
 
+const cars = [{ make: "Lambo" }, { make: "Range Rover" }, { make: "Buggati" }];
+const getAllCars = () => cars;
 export const resolvers = {
-    Query: {
-        hello: (root, args, context) => hello(args, context)
-    }
-}
+  Query: {
+    hello: (root, args, context) => hello(args, context),
+    getAllCars: (root, args, context) => getAllCars(),
+  },
+};
+// { make: "Lambo" }, { make: "Range Rover" }, { make: "Buggati" }
--- src/schema.js
+++ src/schema.js
@@ -1,14 +1,19 @@
 const schema = `
 
 
-
+type Car {
+     make: String!
+} 
 """
 A hello world Query
+
+
 """
 type Query {
      hello: String!
+     getAllCars: [Car]
 }
 
-`
+`;
 
-export {schema}
+export { schema };
